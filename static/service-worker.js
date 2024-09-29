d3dafe02ef31bbb903546acbe0ee0c0fca665f98
sy-contest <181183831+sy-contest@users.noreply.github.com>
2024-09-29 16:00:32 +0300

    fixing console errors

--- static/service-worker.js
+++ static/service-worker.js
@@ -3,20 +3,30 @@ const urlsToCache = [
   '/',
   '/static/styles.css',
   '/static/script.js',
+  '/static/index.html',
   '/static/rps-bgm.mp3',
   '/static/EarnPoint.mp3',
   '/static/LosingPoint.mp3',
-  '/static/default-avatar.png',
-  '/static/game-trophy.png',
   '/static/icon-192x192.png',
-  '/static/icon-512x512.png'
+  '/static/icon-512x512.png',
+  '/static/slider-1.gif',
+  '/static/slider-2.gif',
+  '/static/slider-3.gif',
+  '/static/no-music.png',
+  '/static/music.png',
+  '/static/no-sound.png',
+  '/static/sound.png',
+  '/static/login-logo.png',
+  '/static/menu-logo.png'
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        return cache.addAll(urlsToCache);
+        return cache.addAll(urlsToCache).catch(error => {
+          console.error('Failed to cache some files:', error);
+        });
       })
   );
 });
